7b2c2369baa8f02867edecc93e9b7c36b202303d
KKiranGowtham <KKiranGowtham@KKiranGowtham>
2020-03-23 12:09:49 +0530

    test changes

--- test/src/test/TestClass.java
+++ test/src/test/TestClass.java
@@ -3,5 +3,26 @@ package test;
 public class TestClass {
 	private String a;
 	private String b;
-		
+	private String c;
+	
+	public String getA() {
+		return a;
+	}
+	
+	public void setA(String a) {
+		this.a = a;
+	}
+	
+	public String getB() {
+		return b;
+	}
+	public void setB(String b) {
+		this.b = b;
+	}
+	public String getC() {
+		return c;
+	}
+	public void setC(String c) {
+		this.c = c;
+	}
 }
